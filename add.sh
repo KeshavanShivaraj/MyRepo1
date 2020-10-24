4dc8f627dc8c9dedd1108db43f09047e74a22843
keshavanShivaraj <keshavan.keshavan2002@gmail.com>
2020-10-24 10:35:52 +0530

    modified add

--- add.sh
+++ add.sh
@@ -3,4 +3,6 @@ echo -n 'Enter the First Number: '
 read -r a
 echo -n 'Enter the Second Number: '
 read -r b
-echo "$a + $b = $((a+b))"
+echo -n 'Enter the third Number: '
+read -r c
+echo "$a + $b +$c = $((a+b+c))"
